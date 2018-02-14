6c9d637e4c69cf7c796272e376bcad9bfbc1d0ac
Blake Noll <blakenoll@bnollMBP15-MKT.local>
2018-02-14 13:27:54 -0800

    algolia search

--- js/init.js
+++ js/init.js
@@ -11,3 +11,38 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+var search = instantsearch({
+   // Replace with your own values
+   appId: '2B9RC67QYJ',
+   apiKey: '88450a0a5df5d1f6e7da2db0a56c70a5', // search only API key, no ADMIN key
+   indexName: 'getstarted_actors',
+   urlSync: true,
+   searchParameters: {
+     hitsPerPage: 10
+  }
+});
+
+search.addWidget(
+ instantsearch.widgets.searchBox({
+   container: '#search-input'
+ })
+);
+
+search.addWidget(
+ instantsearch.widgets.hits({
+   container: '#hits',
+   templates: {
+     item: document.getElementById('hit-template').innerHTML,
+     empty: "We didn't find any results for the search <em>\"{{query}}\"</em>"
+   }
+ })
+);
+
+search.addWidget(
+ instantsearch.widgets.pagination({
+   container: '#pagination'
+ })
+);
+
+search.start();
